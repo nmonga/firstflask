e2a2b8dffb0c38acab111dda4b46ff5446cc7ac1
nikhil <nikhil@nikhils-mbp.attlocal.net>
2020-06-25 01:49:03 -0500

    No message

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ def index():
 def about():
   return render_template('about.html')
 
+@app.route('/')
+def plot():
+  return render_template('lines.html')
+
 if __name__ == '__main__':
   app.run(port=33507)
 
